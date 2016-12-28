5374c5d47e014caaded712ef0612b4b27604da42
Patrick Ofilada <pcofilada@gmail.com>
2016-12-28 23:12:22 +0800

    Finish creating third exercise

---
+++ discuss/config/config.exs
@@ -0,0 +1,36 @@
+# This file is responsible for configuring your application
+# and its dependencies with the aid of the Mix.Config module.
+#
+# This configuration file is loaded before any dependency and
+# is restricted to this project.
+use Mix.Config
+
+# General application configuration
+config :discuss,
+  ecto_repos: [Discuss.Repo]
+
+# Configures the endpoint
+config :discuss, Discuss.Endpoint,
+  url: [host: "localhost"],
+  secret_key_base: "IBjw7stDUYMJVMJjymNVZDp5fhBNPW6/whMlUTT2DusAE+7Hnr0gZ0xT8iDTds1z",
+  render_errors: [view: Discuss.ErrorView, accepts: ~w(html json)],
+  pubsub: [name: Discuss.PubSub,
+           adapter: Phoenix.PubSub.PG2]
+
+# Configures Elixir's Logger
+config :logger, :console,
+  format: "$time $metadata[$level] $message\n",
+  metadata: [:request_id]
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
+
+config :ueberauth, Ueberauth,
+  providers: [
+    github: { Ueberauth.Strategy.Github, [] }
+  ]
+
+config :ueberauth, Ueberauth.Strategy.Github.OAuth, 
+  client_id: "9f6efd3bc8426dfb4261",
+  client_secret: "a1dc214738bc279ed76b9d112074e19fc69492c0"
---
+++ discuss/web/router.ex
@@ -0,0 +1,36 @@
+defmodule Discuss.Router do
+  use Discuss.Web, :router
+
+  pipeline :browser do
+    plug :accepts, ["html"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+    plug Discuss.Plugs.SetUser
+  end
+
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/", Discuss do
+    pipe_through :browser # Use the default browser stack
+
+    get "/", TopicController, :index
+    resources "/topics", TopicController, except: [:index]
+  end
+
+  scope "/auth", Discuss do 
+    pipe_through :browser
+
+    get "/signout", AuthController, :signout
+    get "/:provider", AuthController, :request
+    get "/:provider/callback", AuthController, :callback
+  end
+
+  # Other scopes may use custom stacks.
+  # scope "/api", Discuss do
+  #   pipe_through :api
+  # end
+end
